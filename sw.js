f50dd78c1331bee36907ce450fae6768d9606347
demosprojects <kentuckyr2@gmail.com>
2026-06-28 20:16:02 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 
 // ──────────────────────────────────────────────────────────
 
-const CACHE_NAME = "marchese-admin-v9";
+const CACHE_NAME = "marchese-admin-v1.0";
 
 // Rutas relativas a donde está el sw.js (misma carpeta que todo)
 const SHELL_ASSETS = [
